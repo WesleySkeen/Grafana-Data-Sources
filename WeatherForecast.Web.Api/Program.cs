ec131d217b00157035b8d8c07d7c82ad0588fa3d
Wesley Skeen <wesley.skeen@emailage.com>
2022-10-12 10:03:32 +0100

    Run the app on port 5000

--- WeatherForecast.Web.Api/Program.cs
+++ WeatherForecast.Web.Api/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5000);
+});
 
 var app = builder.Build();
 
