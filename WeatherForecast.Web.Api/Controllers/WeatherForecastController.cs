2f7215be5d6016811f9980aca52cd25691fdbbc4
Wesley Skeen <wesley.skeen@emailage.com>
2023-03-09 12:59:50 +0000

    query string

--- WeatherForecast.Web.Api/Controllers/WeatherForecastController.cs
+++ WeatherForecast.Web.Api/Controllers/WeatherForecastController.cs
@@ -24,27 +24,14 @@ public class WeatherForecastController : ControllerBase
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
-    public async Task<IEnumerable<WeatherForecast>> Get()
+    public async Task<string> Get(string data)
     {
         using var activity = SharedTelemetryUtilities.Writer.StartActivity("get_weather_forecasts");
         
         SharedTelemetryUtilities.RequestCounter.Add(1);
-        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        });
-        using (LogContext.PushProperty("app", "api"))
-        {
-            _logger.LogInformation("Help my CC # is 4567-6543-4567-8777");    
-        }
-        // _logger.LogInformation("Max Celsius temperature was {temp}", forecasts.Max(x => x.TemperatureC));
         
-        // External calls to demonstrate tracing
-        // await _httpClient.GetAsync("/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m");
-        // await _httpClient.GetAsync("/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m");
-
-        return forecasts.ToArray();
+        _logger.LogInformation($"my data is {data}"); 
+        
+        return data;
     }
 }
